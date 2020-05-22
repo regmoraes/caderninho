0cd438284af9f681e9dfa4fee29ec6679bfce03e
Rômulo Eduardo G. Moraes <romuloeduardo.moraes@gmail.com>
2020-05-22 18:22:41 -0300

    Remove Drawer header
    Signed-off-by: Rômulo Eduardo G. Moraes <romuloeduardo.moraes@gmail.com>

--- lib/presentation/widgets/navigation_drawer.dart
+++ lib/presentation/widgets/navigation_drawer.dart
@@ -11,14 +11,7 @@ class NavigationDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        padding: EdgeInsets.zero,
         children: <Widget>[
-          DrawerHeader(
-            child: Text('Drawer Header'),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-          ),
           ListTile(
               leading: Icon(Icons.home),
               title: Text('Home'),
